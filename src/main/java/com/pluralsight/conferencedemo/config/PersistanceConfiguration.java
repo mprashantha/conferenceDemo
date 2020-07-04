002bc590958ed8e976bac9ac833297623f6134d3
mprashantha <mprashantha@yahoo.com>
2020-07-04 22:31:28 +0530

    Version 1.0

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistanceConfiguration.java
@@ -0,0 +1,21 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistanceConfiguration {
+/*
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:mysql://localhost:3306/conference_demo");
+        builder.username("root");
+        builder.password("Welcome123");
+        System.out.println("My data source build successful");
+        return builder.build();
+    }*/
+}
